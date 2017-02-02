a8bd6daca407e84a1780040f5378c6322d6a461b
Eder <eder.ramirez87@gmail.com>
2017-02-01 21:23:22 -0600

    metodos de prueba personalisados

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -5,4 +5,19 @@ class FeatureTestCase extends TestCase
 {
 	use DatabaseTransactions;
 
+	function seeErrors(array $fields)
+	{
+
+		foreach ($fields as $name => $errors) {
+			foreach ((array) $errors  as $message) {
+				$this->seeInElement(
+					"#field_{$name} .help-block",
+					$message
+					);
+			}
+		}
+
+		
+	}
+
 }
